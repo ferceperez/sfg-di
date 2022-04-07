c587866d921069bf0ed2223adee01572f3bcdeab
ZeusPC IJ <ferceperez@>
2022-04-07 16:27:16 -0500

    Testing new Branch

--- src/main/java/zeus/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/zeus/springframework/sfgdi/SfgDiApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
 		SpringApplication.run(SfgDiApplication.class, args);
 	}
 
