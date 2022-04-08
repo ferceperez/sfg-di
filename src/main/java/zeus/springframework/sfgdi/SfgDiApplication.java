1121d5574a10da7e8698a0993e8995686df58da9
ZeusPC IJ <ferceperez@>
2022-04-08 09:58:27 -0500

    Application Context example

--- src/main/java/zeus/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/zeus/springframework/sfgdi/SfgDiApplication.java
@@ -2,14 +2,21 @@ package zeus.springframework.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import zeus.springframework.sfgdi.Controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController"); // no hace falta crear el objecto controller,  nunca se usa el new, el appcontext nos trae el bean
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
